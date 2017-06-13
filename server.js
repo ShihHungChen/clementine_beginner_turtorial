fb2acd73c2adec1f0e593b51a0460978f1cb0841
Shi Hung <c.shihhung@gmail.com>
2017-06-13 15:56:31 +0000

    use express.route instead of express.get

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+routes = require('./app/routes/index.js');
 
 var app = express();
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/index.html'); // cwd -> current working directory
-});
+routes(app);
 
 app.listen(8080, function(){
   console.log('Listening on port 8080...');
